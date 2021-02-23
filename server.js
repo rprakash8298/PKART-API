f7f3fafe9a5d9462ceb59a663dda13071a13792e
Ravi Prakash <rprakash1391995@gmail.com>
2021-02-23 11:50:08 +0530

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+require('./db/mongoose')
 const userRouter = require('./routes/user')
 const taskRouter = require('./routes/task')
 const port = process.env.PORT || 5000
-require('./db/mongoose')
 // const User = require('./models/user')
 app.use(express.json())
 app.use(userRouter)
