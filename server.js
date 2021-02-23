6e0c398db3f5a26a3f96feff9f912de583e7d648
Ravi Prakash <rprakash1391995@gmail.com>
2021-02-23 12:02:45 +0530

    final

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const taskRouter = require('./routes/task')
 const port = process.env.PORT || 5000
 // const User = require('./models/user')
 app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.use('/',userRouter)
+app.use('/',taskRouter)
 
 app.listen(port, () => {
     console.log(`app running successfully at ${port}`)
